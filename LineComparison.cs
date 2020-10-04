511e116b11569eef8a6312593c1a1e17945c8011
Kirti Swaraj <Sai@KirtiSwaraj>
2020-10-05 05:03:41 +0530

    [kirti] Add . Line comparison using class

---
+++ LineComparison.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LineComparisonProblem
+    {
+        class LineComparison
+        {
+            double pointX1;
+            double pointY1;
+            double pointX2;
+            double pointY2;
+            public void getCoordinates()
+            {
+                Console.WriteLine("\nEnter Xcoordinate of first point:");
+                pointX1 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enter Ycoordinate of first point:");
+                pointY1 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enter Xcoordinate of second point:");
+                pointX2 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enter Ycoordinate of second point:");
+                pointY2 = Convert.ToDouble(Console.ReadLine());
+            }
+            public double calculateLength()
+            {
+                
+                double power = Math.Pow(pointX2 - pointX1, 2) + Math.Pow(pointY2 - pointY1, 2);
+                double length = Math.Sqrt(power);
+                Console.WriteLine("Length of the line: " + length);
+                return length;
+            }
+            public void checkEquality(LineComparison line1, LineComparison line2)
+            {
+                String firstLine = Convert.ToString(line1.calculateLength());
+                String secondLine = Convert.ToString(line2.calculateLength());
+                Console.WriteLine("Both lines are equal: " + firstLine.Equals(secondLine));
+            }
+            public void compareLines(LineComparison line1, LineComparison line2)
+            {
+                double length1 = line1.calculateLength();
+                double length2 = line2.calculateLength();
+                double compValue = length1.CompareTo(length2);
+                if (compValue < 0)
+                    Console.WriteLine("First line is shorter than the second");
+                else if (compValue == 0)
+                    Console.WriteLine("Both lines are equal");
+                else if (compValue > 0)
+                    Console.WriteLine("First line is longer than the second");
+            }
+        }
+    }
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace LineComparisonProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+         Console.WriteLine("Welcome to Line Comparison Computation Program"+"\n");
+         LineComparison line1 = new LineComparison();
+         LineComparison line2 = new LineComparison();
+            line1.getCoordinates();
+            line2.getCoordinates();
+            line1.calculateLength();
+            line2.calculateLength();
+            line1.checkEquality(line1, line2);
+           line1.compareLines(line1, line2);
+           
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
